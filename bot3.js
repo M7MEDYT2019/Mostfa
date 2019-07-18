c6b8656e584d7148f0f6d7734656029c563da978
Mohamed <42390271+M7MEDYT2019@users.noreply.github.com>
2019-07-18 16:19:29 +0200

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "506883940204675103"; // ايدي السررفر
-var channel = "509340794633715712";//ايدي الروم
+var server = "601331176418050074"; // ايدي السررفر
+var channel = "601362373961580544";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('** محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد **')
     },305);
@@ -16,16 +16,16 @@ var channel = "509340794633715712";//ايدي الروم
 
 
 client.on('ready', async() => {
-var server = "506883940204675103"; // ايدي السررفر
-var channel = "509340794633715712";//ايدي الروم
+var server = "601331176418050074"; // ايدي السررفر
+var channel = "601362400079380510";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('#daily')
     },8.64e+7);
 })
 
 client.on('ready', async() => {
-var server = "506883940204675103"; // ايدي السررفر
-var channel = "509340794633715712";//ايدي الروم
+var server = "601331176418050074"; // ايدي السررفر
+var channel = "601362426797228066";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('#rep <@306464381775118336>')
     },8.64e+7);
@@ -34,7 +34,7 @@ var channel = "509340794633715712";//ايدي الروم
 
 client.on('message', message => {
 if(message.content.startsWith('3s')) {
-if(message.author.id !== "420637382644072451") return;
+if(message.author.id !== "306464381775118336") return;
 var args = message.content.split(' ').slice(1).join(' ');
 message.channel.send(args);
 }
