dd35a0fbf773b831089ee7ae2ea53d27bd31e986
Mohamed <42390271+M7MEDYT2019@users.noreply.github.com>
2019-08-03 00:16:55 +0200

    Update bot10.js

--- bot10.js
+++ bot10.js
@@ -6,8 +6,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "506883940204675103"; // ايدي السررفر
-var channel = "509340824333713418";//ايدي الروم
+var server = "601331176418050074"; // ايدي السررفر
+var channel = "601362400079380510";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('** محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد **')
     },305);
@@ -16,32 +16,54 @@ var channel = "509340824333713418";//ايدي الروم
 
 
 client.on('ready', async() => {
-var server = "506883940204675103"; // ايدي السررفر
-var channel = "509340824333713418";//ايدي الروم
+var server = "601331176418050074"; // ايدي السررفر
+var channel = "601362426797228066";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('#daily')
     },8.64e+7);
 })
 
 client.on('ready', async() => {
-var server = "506883940204675103"; // ايدي السررفر
-var channel = "509340824333713418";//ايدي الروم
+var server = "601331176418050074"; // ايدي السررفر
+var channel = "601362400079380510";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#rep <@314135031029170197>')
+    client.guilds.get(server).channels.get(channel).send('#rep <@306464381775118336>')
     },8.64e+7);
 })
 
 
 client.on('message', message => {
 if(message.content.startsWith('10s')) {
-if(message.author.id !== "420637382644072451") return;
+if(message.author.id !== "306464381775118336") return;
 var args = message.content.split(' ').slice(1).join(' ');
 message.channel.send(args);
 }
 });
  
 
+client.on('message', message => {
+	if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
 
+client.on('message', message => {
+	if(message.content === '2'){
+        message.channel.send('#credit')
+    }
+});
+
+client.on('message', message => {
+	if(message.content === '3'){
+        message.channel.send('#rep <@306464381775118336>')
+    }
+});
+
+client.on('message', message => {
+     if(message.content === '4'){
+        message.member.voiceChannel.join();
+    }
+});
 
 
 
