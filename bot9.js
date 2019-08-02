0269316aa8f5bac9d3272db0c94a28d48406916d
Mohamed <42390271+M7MEDYT2019@users.noreply.github.com>
2019-08-03 00:16:34 +0200

    Create bot9.js

---
+++ bot9.js
@@ -0,0 +1,70 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('ready', async() => {
+var server = "601331176418050074"; // ايدي السررفر
+var channel = "601362400079380510";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('** محمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمدمحمد **')
+    },305);
+})
+
+
+
+client.on('ready', async() => {
+var server = "601331176418050074"; // ايدي السررفر
+var channel = "601362426797228066";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },8.64e+7);
+})
+
+client.on('ready', async() => {
+var server = "601331176418050074"; // ايدي السررفر
+var channel = "601362400079380510";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#rep <@306464381775118336>')
+    },8.64e+7);
+})
+
+
+client.on('message', message => {
+if(message.content.startsWith('9s')) {
+if(message.author.id !== "306464381775118336") return;
+var args = message.content.split(' ').slice(1).join(' ');
+message.channel.send(args);
+}
+});
+ 
+
+client.on('message', message => {
+	if(message.content === '1'){
+        message.channel.send('#daily')
+    }
+});
+
+client.on('message', message => {
+	if(message.content === '2'){
+        message.channel.send('#credit')
+    }
+});
+
+client.on('message', message => {
+	if(message.content === '3'){
+        message.channel.send('#rep <@306464381775118336>')
+    }
+});
+
+client.on('message', message => {
+     if(message.content === '4'){
+        message.member.voiceChannel.join();
+    }
+});
+
+
+
+client.login(process.env.BOT_TOKEN9); 
