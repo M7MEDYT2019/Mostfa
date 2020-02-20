6714ed1acf459f7887053ffa62c6dafa55fb58bc
Mohamed <42390271+M7MEDYT2019@users.noreply.github.com>
2020-02-20 14:07:01 +0200

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -6,19 +6,18 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "493627472647028745"; // ايدي السررفر
-var channel = "493628074685104130";//ايدي الروم
+var server = "670751289705234445"; // ايدي السررفر
+var channel = "670751536883826744";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('** Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة Denver Lozano Is Here بيلين شرموطة  Denver Lozano Is Here بيلين شرموطة **')
+    client.guilds.get(server).channels.get(channel).send('** انتا مين عشان تزعل اعز صاحب ليا حطو في Ass بتاعتك يا سنايبر :) **')
     },305);
 })
 
 
 
-
 client.on('ready', async() => {
 var server = "601331176418050074"; // ايدي السررفر
-var channel = "601362426797228066";//ايدي الروم
+var channel = "601362400079380510";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('#daily')
     },8.64e+7);
@@ -28,20 +27,19 @@ client.on('ready', async() => {
 var server = "601331176418050074"; // ايدي السررفر
 var channel = "601362400079380510";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#rep <@306464381775118336>')
+    client.guilds.get(server).channels.get(channel).send('#rep <@314135031029170197>')
     },8.64e+7);
 })
 
 
 client.on('message', message => {
-if(message.content.startsWith('2s')) {
+if(message.content.startsWith('1s')) {
 if(message.author.id !== "306464381775118336") return;
 var args = message.content.split(' ').slice(1).join(' ');
 message.channel.send(args);
 }
 });
  
-
 client.on('message', message => {
 	if(message.content === '1'){
         message.channel.send('#daily')
@@ -65,6 +63,7 @@ client.on('message', message => {
         message.member.voiceChannel.join();
     }
 });
+
 
 
 
